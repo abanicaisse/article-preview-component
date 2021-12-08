0f8d7feccec2fcb80c9aefaf1f6604893176beb8
Aba Wandjovu Nicaisse <abawandjovunicaise@gmail.com>
2021-12-08 07:04:20 +0300

    initial commit and styled mobile version

---
+++ main.js
@@ -0,0 +1,12 @@
+// const cilckBtn = document.getElementById("click");
+
+// cilckBtn.addEventListener("click", function(e){
+//     if (window.innerWidth < 1000) {
+//         console.log("small screen");
+//     } else {
+//         console.log("big screen");
+//     }
+// });
+
+
+// console.log(cilckBtn.textContent);
