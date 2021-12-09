22ef5271f8758ab1506725317f5aa1ab64ad1ca7
Aba Wandjovu Nicaisse <abawandjovunicaise@gmail.com>
2021-12-09 19:17:54 +0300

    created sharing-options animations for mobile

--- main.js
+++ main.js
@@ -3,29 +3,34 @@ const shareSection = document.querySelector(".description__share");
 
 shareBtn.addEventListener("click", showOptions);
 
-function showOptions(e){
-    const shareOptions = document.querySelector(".share-options");
-    //define the share box we want for mobile and tablets
-    if (window.innerWidth < 1000) {
-        const clientDetail = document.querySelector(".client-info");
-    
-        shareOptions.classList.toggle("mobile-share");
-        //style the box accordingly to the design
-        shareSection.classList.toggle("description__share--active")
+function showOptions(e) {
+  const shareOptions = document.querySelector(".share-options");
+  //define the share box we want for mobile and tablets
+  if (window.innerWidth < 1000) {
+    const clientDetail = document.querySelector(".client-info");
 
-        if (clientDetail.style.display === "none") {
-            clientDetail.style.display = "flex";
-        } else {
-            clientDetail.style.display = "none";
-        }
-        //If the desktop sharing box has been activated previously we deactivate it
-        shareOptions.classList.remove("desktop-share");
+    shareOptions.classList.toggle("mobile-share");
+    //style the box accordingly to the design
+    shareSection.classList.toggle("description__share--active");
+
+    if (clientDetail.style.display === "none") {
+      clientDetail.style.display = "flex";
+      clientDetail.style.animation = "return 1s ease-out";
+      shareSection.style.animation = "return 5s ease";
     } else {
-        shareOptions.classList.toggle("desktop-share");
-        const clientDetail = document.querySelector(".client-info"); clientDetail.style.display = "flex";
-
-        //If the mobile sharing box has been activated previously we deactivate and also remove all its stylings
-        shareOptions.classList.remove("mobile-share");
-        shareSection.classList.remove("description__share--active");
+      clientDetail.style.display = "none";
     }
+    //If the desktop sharing box has been activated previously we deactivate it
+    shareOptions.classList.remove("desktop-share");
+    
+    //define the share box we want for desktops
+  } else {
+    shareOptions.classList.toggle("desktop-share");
+    const clientDetail = document.querySelector(".client-info");
+    clientDetail.style.display = "flex";
+
+    //If the mobile sharing box has been activated previously we deactivate and also remove all its stylings
+    shareOptions.classList.remove("mobile-share");
+    shareSection.classList.remove("description__share--active");
+  }
 }
